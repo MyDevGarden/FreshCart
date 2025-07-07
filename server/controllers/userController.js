74eaa3fa0cecea37fdf56226aa828f6429724bbc
MyDevGarden <nilima.raundal@gmail.com>
2025-07-07 12:14:48 +0530

    Initial web server setup and User Api created

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+import User from "../models/Users.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+//Register User : /api/user/register
+
+export const register = async (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+
+        if(!name || !email || ! password) {
+            return res.json({success: false, message: 'Missing Details'})
+        }
+
+        const existingUser = await User.findOne({email})
+
+        if(existingUser)
+            return res.json({success: false, message: 'User allready exists'})
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({name, email, password: hashedPassword})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+
+        res.cookie('token', token, {
+            httpOnly: true,  //prevent javascript to access cookie
+            secure: process.env.NODE_ENV === 'production', //Use secure cookie in production environment
+            sameSite : process.env.NODE_ENV === 'production' ? 'none' : 'strict', //CSRF protection
+            maxAge: 7 * 24 * 60 * 1000, // 7 days expiry of coolie
+        })
+
+        return  res.json({success: true, user: {email: user.email, name: user.name}})
+
+    } catch (error) {
+        console.log(error.message);
+        
+        res.json({success: false, message: error.message})
+    }
+}
